5a9555c90ac00452f5de34222a683fadbb251547
Toth <kristof.toth@sanmina.com>
2024-03-01 13:15:13 +0100

    nav menü jogosultságkezelés kész

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,16 +1,24 @@
+//const {views} = await import(`../Router/views.js${app_version}`)
 const {views} = await import(`../Router/views.js${app_version}`)
 const {$_GET} = await import(`../../Hooks/findGET/findGET.js${app_version}`)
+const {ajax} = await import(`../../Hooks/ajax/ajax.js${app_version}`)
 export const NavBar = () => {
     const root = document.getElementById("root")
-
+    const userID = ajax("get", "./server/activeUser/activeUser.php", "json");
     const activeView = $_GET.view == null ? "Home" : $_GET.view
+    document.title = views[activeView].title
 
     let context = ""
     Object.keys(views).forEach((key)=>{
-        context += `<a href="?view=${key}">${key}</a>`
+        const activeWindow = key == $_GET.view ? "active" : ""
+        if(views[key].login == false || (views[key].login == true && userID != null) ) {
+            context += `<a class="${activeWindow}" href="?view=${key}">${views[key].title}</a>`
+        }
+
+        //console.log(`view: ${views[key].login} userID: ${userID != null}`)
     })
 
-    const viewContext =  views[activeView]()
+    const viewContext =  views[activeView].fnc({userID:userID})
     const output = viewContext.DOM != null ?   viewContext.DOM : viewContext
 
     root.innerHTML = `
--- src/views/OpenTopic/OpenTopic.js
+++ src/views/OpenTopic/OpenTopic.js
@@ -1,21 +1,21 @@
 const {Topic} = await import(`./Topic.js${app_version}`)
 const {events} = await import(`./events.js${app_version}`)
-const userID = 270287
 const {createHeader, buildFeed, createTable} = await import(`./functions.js${app_version}`)
 
-export const OpenTopic = () =>{
-
+export const OpenTopic = (props) =>{
+    const userID = props.userID == null ? 0:props.userID
+    //console.log(userID)
     const pageContent = document.getElementById("page-content")
     let context = {}
 
     if(Topic != false && typeof Topic != "undefined"){
-        context.DOM = createTable(userID) 
-        context.events = events
+        context.DOM = createTable(userID)
+        context.events = props.userID == null ? () => {} : events
         context.Topic = Topic
     }
     else{
         context.DOM = `<div>A keresett topic nem található</div>` 
-        context.events = null
+        context.events = () => {}
     }
     return context
 }
